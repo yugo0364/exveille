475dd694eb1366a183aaea69d0b429573b391e8d
yugo <yugo0364@yahoo.ca>
2017-03-21 14:25:49 -0400

    debut examen et etape1

---
+++ exam2/etape.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const express = require('express');
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID;
+const app = express();
+app.set('view engine', 'ejs'); // générateur de template 
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))  // pour utiliser le dossier public
+app.use(bodyParser.json())  // pour traiter les données JSON
+
+
+
